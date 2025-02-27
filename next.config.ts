b691f8bb2d5d38d1e25eba4fcf5078fefff9fb2b
Manoj Chopkar <Espire.Manoj.Chopkar.Admin@myacu.onmicrosoft.com>
2025-02-27 12:46:56 +0530

    Add config changes

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,11 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'contentful-accelerator.netlify.app',
+        port: '',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.ctfassets.net',
         port: '',
       },
